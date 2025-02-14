8a76d7e50c8154638cb2b6e7db38fd70ccbee95b
lukeschmitt-tr <85308904+lukeschmitt-tr@users.noreply.github.com>
2025-02-13 20:07:35 -0600

    Deploy v1.3.2 (#7)
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- include/libtrossen_arm/trossen_arm_config.hpp
+++ include/libtrossen_arm/trossen_arm_config.hpp
@@ -32,6 +32,6 @@
 // Version passed from CMakeLists.txt
 #define DRIVER_VERSION_MAJOR 1
 #define DRIVER_VERSION_MINOR 3
-#define DRIVER_VERSION_PATCH 1
+#define DRIVER_VERSION_PATCH 2
 
 #endif  // LIBTROSSEN_ARM__TROSSEN_ARM_CONFIG_HPP_
